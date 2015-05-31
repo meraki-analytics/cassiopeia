713d37470f70acdc4d88fccf94b663113ec1deee
Rob Rua <robrua@alumni.cmu.edu>
2015-05-30 21:07:08 -0400

    Finished rate limiter

--- cassiopeia/baseriotapi.py
+++ cassiopeia/baseriotapi.py
@@ -1,4 +1,5 @@
 from cassiopeia import requests
+from cassiopeia.type.api.rates import SingleRateLimiter, MultiRateLimiter
 from cassiopeia.dto.championapi import *
 from cassiopeia.dto.leagueapi import *
 from cassiopeia.dto.staticdataapi import *
@@ -22,3 +23,12 @@ def set_mirror(mirror):
 # @param on # bool # Whether to print calls as they are made to the API
 def print_calls(on):
     requests.print_calls = on
+
+# @param calls_per_epoch # int # Number of calls allowed in each epoch
+# @param seconds_per_epoch # int # Number of seconds per epoch
+def set_rate_limit(calls_per_epoch, seconds_per_epoch):
+    requests.rate_limiter = SingleRateLimiter(calls_per_epoch, seconds_per_epoch)
+
+# @param limits # list<tuple> # A list of rate limit pairs. A rate limit is (calls_per_epoch, seconds_per_epoch)
+def set_rate_limit(limits):
+    requests.rate_limiter = MultiRateLimiter(limits)
