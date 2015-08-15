88f6b8cb614288bc83b8c00203e9245810a42ed9
Rob Rua <robertrua@gmail.com>
2015-08-15 17:47:55 -0400

    League DTO API was old as hell for some reason. Fixed it.

--- cassiopeia/dto/leagueapi.py
+++ cassiopeia/dto/leagueapi.py
@@ -1,5 +1,5 @@
-from cassiopeia.dto import requests
-from cassiopeia.type.dto.league import *
+import cassiopeia.dto.requests
+import cassiopeia.type.dto.league
 
 # @param summoner_ids # list<int> or int # A list of summoner IDs to get leagues for or a single ID
 # @return # dict<str, list<League>> # A mapping of summoner IDs to the leagues the summoner is in
@@ -11,13 +11,13 @@ def get_leagues_by_summoner(summoner_ids):
     id_string = ",".join(str(x) for x in summoner_ids) if isinstance(summoner_ids, list) else str(summoner_ids)
 
     # Get JSON response
-    request = "{version}/league/by-summoner/{ids}".format(version=requests.api_versions["league"], ids=id_string)
-    response = requests.get(request)
+    request = "{version}/league/by-summoner/{ids}".format(version=cassiopeia.dto.requests.api_versions["league"], ids=id_string)
+    response = cassiopeia.dto.requests.get(request)
 
     # Convert response to Dto type
     for id_, leagues in response.items():
         for i, league in enumerate(leagues):
-            leagues[i] = League(league)
+            leagues[i] = cassiopeia.type.dto.league.League(league)
 
     return response
 
@@ -31,13 +31,13 @@ def get_league_entries_by_summoner(summoner_ids):
     id_string = ",".join(str(x) for x in summoner_ids) if isinstance(summoner_ids, list) else str(summoner_ids)
 
     # Get JSON response
-    request = "{version}/league/by-summoner/{ids}/entry".format(version=requests.api_versions["league"], ids=id_string)
-    response = requests.get(request)
+    request = "{version}/league/by-summoner/{ids}/entry".format(version=cassiopeia.dto.requests.api_versions["league"], ids=id_string)
+    response = cassiopeia.dto.requests.get(request)
 
     # Convert response to Dto type
     for id_, leagues in response.items():
         for i, league in enumerate(leagues):
-            leagues[i] = League(league)
+            leagues[i] = cassiopeia.type.dto.league.League(league)
 
     return response
 
@@ -51,13 +51,12 @@ def get_leagues_by_team(team_ids):
     id_string = ",".join(team_ids) if isinstance(team_ids, list) else str(team_ids)
 
     # Get JSON response
-    request = "{version}/league/by-team/{ids}".format(version=requests.api_versions["league"], ids=id_string)
-    response = requests.get(request)
+    request = "{version}/league/by-team/{ids}".format(version=cassiopeia.dto.requests.api_versions["league"], ids=id_string)
+    response = cassiopeia.dto.requests.get(request)
 
     # Convert response to Dto type
     for id_, leagues in response.items():
-        for i, league in enumerate(leagues):
-            leagues[i] = League(league)
+        response[id_] = [cassiopeia.type.dto.league.League(league) for league in leagues]
 
     return response
 
@@ -71,24 +70,23 @@ def get_league_entries_by_team(team_ids):
     id_string = ",".join(team_ids) if isinstance(team_ids, list) else str(team_ids)
 
     # Get JSON response
-    request = "{version}/league/by-team/{ids}/entry".format(version=requests.api_versions["league"], ids=id_string)
-    response = requests.get(request)
+    request = "{version}/league/by-team/{ids}/entry".format(version=cassiopeia.dto.requests.api_versions["league"], ids=id_string)
+    response = cassiopeia.dto.requests.get(request)
 
     # Convert response to Dto type
     for id_, leagues in response.items():
-        for i, league in enumerate(leagues):
-            leagues[i] = League(league)
+        response[id_] = [cassiopeia.type.dto.league.League(league) for league in leagues]
 
     return response
 
 # @param queue_type # str # The queue type to get the challenger league for ("RANKED_SOLO_5x5", "RANKED_TEAM_3x3", "RANKED_TEAM_5x5")
 # @return # League # The challenger league
 def get_challenger(queue_type):
-    request = "{version}/league/challenger".format(version=requests.api_versions["league"])
-    return League(requests.get(request, {"type": queue_type}))
+    request = "{version}/league/challenger".format(version=cassiopeia.dto.requests.api_versions["league"])
+    return cassiopeia.type.dto.league.League(cassiopeia.dto.requests.get(request, {"type": queue_type}))
 
 # @param queue_type # str # The queue type to get the master league for ("RANKED_SOLO_5x5", "RANKED_TEAM_3x3", "RANKED_TEAM_5x5")
 # @return # League # The master league
 def get_master(queue_type):
-    request = "{version}/league/master".format(version=requests.api_versions["league"])
-    return League(requests.get(request, {"type": queue_type}))
+    request = "{version}/league/master".format(version=cassiopeia.dto.requests.api_versions["league"])
+    return cassiopeia.type.dto.league.League(cassiopeia.dto.requests.get(request, {"type": queue_type}))
