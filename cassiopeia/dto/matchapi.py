1da5b68f1abf483130911e1e445e6d0d4dc7f9e6
Rob Rua <robertrua@gmail.com>
2015-09-19 11:44:04 -0400

    docs and expose match timeline option for core

--- cassiopeia/dto/matchapi.py
+++ cassiopeia/dto/matchapi.py
@@ -1,12 +1,13 @@
 import cassiopeia.dto.requests
 import cassiopeia.type.dto.match
 
-def get_match(id_, includeTimeline=True):
+def get_match(id_, include_timeline=True):
     """https://developer.riotgames.com/api/methods#!/1014/3442
 
-    id_       int            the ID of the match to get
+    id_                 int            the ID of the match to get
+    include_timeline    bool           whether to include timeline data in the returned match
 
-    return    MatchDetail    the match
+    return              MatchDetail    the match
     """
     request = "{version}/match/{id_}".format(version=cassiopeia.dto.requests.api_versions["match"], id_=id_)
-    return cassiopeia.type.dto.match.MatchDetail(cassiopeia.dto.requests.get(request, {"includeTimeline": includeTimeline}))
+    return cassiopeia.type.dto.match.MatchDetail(cassiopeia.dto.requests.get(request, {"includeTimeline": include_timeline}))
