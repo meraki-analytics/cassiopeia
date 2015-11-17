26bb8bed0d00e069296be0bebaa9ee8180ec936c
Rob Rua <robertrua@gmail.com>
2015-11-16 22:07:29 -0500

    Version number update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cassiopeia",
-    version="0.0.7",
+    version="0.0.8",
     author="Rob Rua",
     author_email="robrua@alumni.cmu.edu",
     url="https://github.com/robrua/cassiopeia",
