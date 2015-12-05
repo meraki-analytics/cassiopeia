8283b9a0b4d628758c85f1cb6361f6e0d0861e0a
Eric Carmichael <eric@ckcollab.com>
2015-12-05 05:06:32 -0800

    fixes issue #25 python 27 strings when setting region

--- cassiopeia/riotapi.py
+++ cassiopeia/riotapi.py
@@ -35,7 +35,7 @@ def set_region(region):
 
     region    str | cassiopeia.type.core.common.Region    the region to query against
     """
-    if(isinstance(region, str)):
+    if(isinstance(region, str) or isinstance(region, unicode)):
         region = cassiopeia.type.core.common.Region(region.lower())
     cassiopeia.dto.requests.region = region.value
 
