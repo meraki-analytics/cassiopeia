66755430f4937c06744f36aa6e95ff1800754cb4
jason <jjmaldonis@gmail.com>
2015-12-06 18:40:54 -0600

    minor fix to %s for item stats so they return fractional values more consistently rather than percents

--- cassiopeia/type/core/staticdata.py
+++ cassiopeia/type/core/staticdata.py
@@ -757,6 +757,9 @@ class ItemStats(cassiopeia.type.core.common.CassiopeiaObject):
 
     def __init__(self, data, scraped_stats={}):
         super().__init__(data)
+        for k,v in scraped_stats.items():
+            if 'percent' in k and v > 1.0:
+                scraped_stats[k] = v/100.0
         self.__scraped_stats = scraped_stats
 
     def __str__(self):
