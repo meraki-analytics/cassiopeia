97927ada125225dba69bf4d30248adf548d788f0
Eric Carmichael <eric@ckcollab.com>
2015-12-09 13:47:16 -0800

    hrm test passed on 3 when it shouldn't have?

--- cassiopeia/tests/test_python_2_and_3.py
+++ cassiopeia/tests/test_python_2_and_3.py
@@ -4,4 +4,7 @@ from unittest import TestCase
 class Python2And3Tests(TestCase):
 
     def test_we_can_import_cass(self):
-        import cassiopeia
+        from cassiopeia import riotapi
+        riotapi.set_region("NA")
+
+        assert True
