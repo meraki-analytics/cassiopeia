13d14427a3893d3126f0cfad048d62a7d72b8225
Eric Carmichael <eric@ckcollab.com>
2015-12-10 03:32:10 -0800

    revert merging in old stuff

--- cassiopeia/dto/featuredgamesapi.py
+++ cassiopeia/dto/featuredgamesapi.py
@@ -6,5 +6,5 @@ def get_featured_games():
 
     return    FeaturedGames    the current featured game list
     """
-    request = "https://{server}.api.pvp.net/observer-mode/rest/featured".format(server=cassiopeia.dto.requests.region)
+    request = "https://{server}.api.pvp.net/observer-mode/rest/featured".format(server=cassiopeia.dto.requests.region).lower()
     return cassiopeia.type.dto.featuredgames.FeaturedGames(cassiopeia.dto.requests.get(request, include_base=False))
