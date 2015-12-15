978da3427e1ecf0fc7d20f91e58a28240422082e
Eric Carmichael <eric@ckcollab.com>
2015-12-14 17:19:55 -0800

    trying to make better generic str python 2 handlin

--- cassiopeia/__init__.py
+++ cassiopeia/__init__.py
@@ -1,5 +1,18 @@
 import sys
 
-if(sys.version_info.major == 2):
-	import future.standard_library
-	future.standard_library.install_aliases()
+if (sys.version_info.major == 2):
+    import future.standard_library
+
+    future.standard_library.install_aliases()
+
+    # __all__ = ('basestring')
+    # # import __builtin__
+    # # __builtin__.str = unicode
+    # basestring = str
+    # str = unicode
+    #
+    # python 3 renames str -> bytes and unicode -> str
+    # #str = unicode
+    # from future.builtins.misc import super
+    import __builtin__
+    __builtin__.str = unicode
--- cassiopeia/tests/python2_compatibility.py
+++ cassiopeia/tests/python2_compatibility.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 
+from cassiopeia import riotapi
+
 
 class Python2And3Tests(TestCase):
 
-    def test_we_can_import_cass(self):
-        from cassiopeia import riotapi
+    def test_we_can_use_unicode(self):
         riotapi.set_region(u"NA")
-
         assert True
