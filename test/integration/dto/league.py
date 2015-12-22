a978e67c524cafdf493bf676cef33e86b77e4136
Eric Carmichael <eric@ckcollab.com>
2015-12-22 14:58:15 -0800

    pep8 fixes

--- test/integration/dto/league.py
+++ test/integration/dto/league.py
@@ -1,6 +1,7 @@
 from cassiopeia import baseriotapi
 from .. import int_test_handler
 
+
 def test_all():
     print("dto/leagueapi tests...")
     test_leagues_by_summoner()
@@ -10,20 +11,26 @@ def test_all():
     test_challenger()
     test_master()
 
+
 def test_leagues_by_summoner():
     int_test_handler.test_result(baseriotapi.get_leagues_by_summoner(int_test_handler.summoner_id))
+
 
 def test_league_entries_by_summoner():
     int_test_handler.test_result(baseriotapi.get_league_entries_by_summoner(int_test_handler.summoner_id))
 
+
 def test_leagues_by_team():
     int_test_handler.test_result(baseriotapi.get_leagues_by_team(int_test_handler.team_id))
+
 
 def test_league_entries_by_team():
     int_test_handler.test_result(baseriotapi.get_league_entries_by_team(int_test_handler.team_id))
 
+
 def test_challenger():
     int_test_handler.test_result(baseriotapi.get_challenger("RANKED_SOLO_5x5"))
+
 
 def test_master():
     int_test_handler.test_result(baseriotapi.get_master("RANKED_SOLO_5x5"))
