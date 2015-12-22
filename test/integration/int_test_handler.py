dfdde0811c028cea6b36613acc5b8fd3496d5c10
Eric Carmichael <eric@ckcollab.com>
2015-12-22 13:56:58 -0800

    int tests kind of working, use RIOT_API_KEY env var to store key

---
+++ test/integration/int_test_handler.py
@@ -0,0 +1,26 @@
+import pytest
+import os
+
+from cassiopeia import riotapi
+from cassiopeia.type.core.common import LoadPolicy
+
+riotapi.set_api_key(os.environ["RIOT_API_KEY"])
+riotapi.set_region("NA")
+riotapi.print_calls(True)
+riotapi.set_load_policy(LoadPolicy.lazy)
+
+champion_id = 35
+champion_name = "Thresh"
+mastery_id = 6361
+match_id = 1505030444
+rune_id = 5234
+summoner_id = 22508641
+summoner_name = "FatalElement"
+summoner_spell_id = 7
+team_id = "TEAM-49fc9f10-1290-11e3-80a6-782bcb4d0bb2"
+item_id = 3031
+
+
+
+def test_result(result):
+    pass  # assert result
