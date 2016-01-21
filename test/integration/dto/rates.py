1096c5338c7dfe251bf9b6a54e82bb6147b6b6c0
Rob Rua <robertrua@gmail.com>
2016-01-21 02:01:01 -0500

    flake8 rates changes and add riotapi call tracking

--- test/integration/dto/rates.py
+++ test/integration/dto/rates.py
@@ -9,12 +9,13 @@ def test_all():
 
 def test_tracking_on_valid_request():
     """Checks that the request tracking works on requests with expected return code of 200"""
-    int_test_handler.test_result(baseriotapi.get_requests_count() == (0,0))
+    int_test_handler.test_result(baseriotapi.get_requests_count() == (0, 0))
     int_test_handler.test_result(baseriotapi.get_match(int_test_handler.match_id))
-    int_test_handler.test_result(baseriotapi.get_requests_count() == (1,1))
+    int_test_handler.test_result(baseriotapi.get_requests_count() == (1, 1))
+
 
 def test_tracking_on_404_request():
     """Checks that the request tracking works on requests which raise exceptions"""
-    int_test_handler.test_result(baseriotapi.get_requests_count() == (0,0))
+    int_test_handler.test_result(baseriotapi.get_requests_count() == (0, 0))
     int_test_handler.test_result(baseriotapi.get_champion(int_test_handler.non_existent_champion_id))
-    int_test_handler.test_result(baseriotapi.get_requests_count() == (0,1))
+    int_test_handler.test_result(baseriotapi.get_requests_count() == (0, 1))
