8fce996524a18524c86e5455928e9766b06eaa2a
Rob Rua <robertrua@gmail.com>
2016-01-21 02:15:39 -0500

    fix rates tests

--- test/integration/dto/rates.py
+++ test/integration/dto/rates.py
@@ -2,20 +2,18 @@ from cassiopeia import baseriotapi
 from .. import int_test_handler
 
 
-def test_all():
-    print("dto/requests tests...")
-    test_tracking_on_valid_request()
-
-
 def test_tracking_on_valid_request():
     """Checks that the request tracking works on requests with expected return code of 200"""
-    int_test_handler.test_result(baseriotapi.get_requests_count() == (0, 0))
+    start_count = baseriotapi.get_requests_count()
     int_test_handler.test_result(baseriotapi.get_match(int_test_handler.match_id))
-    int_test_handler.test_result(baseriotapi.get_requests_count() == (1, 1))
+    int_test_handler.test_result(baseriotapi.get_requests_count() == (start_count[0] + 1, start_count[1] + 1))
 
 
 def test_tracking_on_404_request():
     """Checks that the request tracking works on requests which raise exceptions"""
-    int_test_handler.test_result(baseriotapi.get_requests_count() == (0, 0))
-    int_test_handler.test_result(baseriotapi.get_champion(int_test_handler.non_existent_champion_id))
-    int_test_handler.test_result(baseriotapi.get_requests_count() == (0, 1))
+    start_count = baseriotapi.get_requests_count()
+    try:
+        baseriotapi.get_champion(int_test_handler.non_existent_champion_id)
+    except:
+        pass
+    int_test_handler.test_result(baseriotapi.get_requests_count() == (start_count[0], start_count[1] + 1))
