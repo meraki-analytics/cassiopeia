b4942fdab99ab3e062d96b30fc86721c7922462e
Rob Rua <robertrua@gmail.com>
2016-11-11 16:42:23 -0500

    New season queue and season enum updates

--- cassiopeia/core/statsapi.py
+++ cassiopeia/core/statsapi.py
@@ -16,9 +16,6 @@ def get_ranked_stats(summoner, season=None):
     Returns:
         dict<Champion, AggregatedStats>: the summoner's ranked stats divided by champion. The entry for None contains combined stats for all champions.
     """
-    if season and season not in cassiopeia.type.core.common.stats_seasons:
-        raise ValueError("Must use a valid season to get ranked stats for")
-
     stats = cassiopeia.dto.statsapi.get_ranked_stats(summoner.id, season.value if season else None)
     champions = {champion.id: champion for champion in cassiopeia.riotapi.get_champions_by_id(list(stats.champion_ids))}
     champions[0] = None
@@ -37,8 +34,5 @@ def get_stats(summoner, season=None):
     Returns:
         dict<StatSummaryType, StatsSummary>: the summoner's stats divided by queue type
     """
-    if season and season not in cassiopeia.type.core.common.stats_seasons:
-        raise ValueError("Must use a valid season to get stats for")
-
     stats = cassiopeia.dto.statsapi.get_stats(summoner.id, season.value if season else None)
     return {cassiopeia.type.core.common.StatSummaryType(summary.playerStatSummaryType): cassiopeia.type.core.stats.StatsSummary(summary) for summary in stats.playerStatSummaries}
