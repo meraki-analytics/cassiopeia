c86e2c59451c3c517aacb34ffe8033c00d26ee05
Volcyy <Johannes.christ@gmail.com>
2017-07-01 00:03:09 +0200

    Make champion.py a bit more readable:
    - Change function name from `test_cass` to `get_champions`
    - Put spaces between start of comments and their contents
    - Remove second `print(annie.title)` call
    - Remove `print()` call. If this was made with the pure intention of inserting a newline, it's also possible to specify `end='\n\n'` in the previous `print` call or simply append a `\n` to it.

--- examples/champion.py
+++ examples/champion.py
@@ -1,29 +1,27 @@
 import cassiopeia as cass
 from cassiopeia.core import Champion
 
-def test_cass():
-    #annie = Champion(name="Annie", region="NA")
+
+def get_champions():
+    # annie = Champion(name="Annie", region="NA")
     annie = Champion(name="Annie")
     print(annie.name)
-    print(annie.title)
     print(annie.title)
     for spell in annie.spells:
         print(spell.name, spell.keywords)
 
     print(annie.info.difficulty)
     print(annie.passive.name)
-    #print(annie.recommended_itemsets[0].item_sets[0].items)
+    # print(annie.recommended_itemsets[0].item_sets[0].items)
     print(annie.free_to_play)
     print(annie._Ghost__all_loaded)
     print(annie)
 
-    print()
-
-    #ziggs = cass.get_champion(region="NA", "Ziggs")
+    # ziggs = cass.get_champion(region="NA", "Ziggs")
     ziggs = cass.get_champion("Ziggs")
     print(ziggs.name)
     print(ziggs.region)
-    #print(ziggs.recommended_itemset[0].item_sets[0].items)
+    # print(ziggs.recommended_itemset[0].item_sets[0].items)
     print(ziggs.free_to_play)
     for spell in ziggs.spells:
         for var in spell.variables:
@@ -32,4 +30,4 @@ def test_cass():
 
 
 if __name__ == "__main__":
-    test_cass()
+    get_champions()
