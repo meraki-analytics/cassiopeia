167c97b2b459591bc1f336a66c5300c933c076d3
Volcyy <Johannes.christ@gmail.com>
2017-07-03 15:51:12 +0200

    Add region as parameter

--- examples/rune_pages.py
+++ examples/rune_pages.py
@@ -2,9 +2,8 @@ import cassiopeia as cass
 from cassiopeia.core import Summoner
 
 
-def print_rune_pages(summoner_name: str):
-
-    summoner = Summoner(name=summoner_name)
+def print_rune_pages(summoner_name: str, region: str):
+    summoner = Summoner(name=summoner_name, region=region)
     print("Name:", summoner.name)
     print("ID:", summoner.id)
 
@@ -19,4 +18,4 @@ def print_rune_pages(summoner_name: str):
 
 
 if __name__ == "__main__":
-    print_rune_pages("Kalturi")
+    print_rune_pages("Kalturi", "NA")
