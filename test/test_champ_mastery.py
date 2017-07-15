2cdfd5479bdf6f2ea4ade100ed63a73535212974
Volcyy <Johannes.christ@gmail.com>
2017-07-15 13:54:07 +0200

    use sets to compare regardless of order, add test for get_champion_mastery

--- test/test_champ_mastery.py
+++ test/test_champ_mastery.py
@@ -20,9 +20,9 @@ def test_raises_with_unknown_summoner():
 def test_masteries_with_id_or_summoner():
     summ = cassiopeia.get_summoner('Kalturi')
 
-    from_summoner = cassiopeia.get_champion_masteries(summ)
-    from_id = cassiopeia.get_champion_masteries(summ.id)
-    from_name = cassiopeia.get_champion_masteries(summ.name)
+    from_summoner = set(cassiopeia.get_champion_masteries(summ))
+    from_id = set(cassiopeia.get_champion_masteries(summ.id))
+    from_name = set(cassiopeia.get_champion_masteries(summ.name))
 
     assert from_summoner == from_id
     assert from_id == from_name
@@ -37,3 +37,12 @@ def test_mastery_return():
 
     assert champ_mastery.summoner.name == 'Kalturi'
     assert champ_mastery.champion.name == 'Karma'
+
+
+def test_mastery_returns_correct_data():
+    masteries = cassiopeia.get_champion_masteries('Kalturi')
+    on_champ = masteries[0]
+
+    from_single_call = cassiopeia.get_champion_mastery('Kalturi', on_champ.champion)
+
+    assert on_champ == from_single_call
