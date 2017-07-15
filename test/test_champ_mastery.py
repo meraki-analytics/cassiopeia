be4f6b5ce656e89d73f4bbfc1adc01b87e02f792
Volcyy <Johannes.christ@gmail.com>
2017-07-15 11:32:55 +0200

    added tests for return type of get_champion_mastery

--- test/test_champ_mastery.py
+++ test/test_champ_mastery.py
@@ -26,3 +26,14 @@ def test_masteries_with_id_or_summoner():
 
     assert from_summoner == from_id
     assert from_id == from_name
+
+
+def test_mastery_return():
+    champ_mastery = cassiopeia.get_champion_mastery('Kalturi', 'Karma')
+
+    assert isinstance(champ_mastery, cassiopeia.ChampionMastery)
+    assert isinstance(champ_mastery.summoner, cassiopeia.Summoner)
+    assert isinstance(champ_mastery.champion, cassiopeia.Champion)
+
+    assert champ_mastery.summoner.name == 'Kalturi'
+    assert champ_mastery.champion.name == 'Karma'
