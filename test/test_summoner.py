8e321203abdaf2f832dd08174c97b50d59c2ecfa
Volcyy <Johannes.christ@gmail.com>
2017-07-29 18:02:38 +0200

    Use the exists attribute to validate that summoners don't exist

--- test/test_summoner.py
+++ test/test_summoner.py
@@ -6,26 +6,13 @@ from datapipelines.common import NotFoundError
 from .constants import SUMMONER_NAME, UNKNOWN_SUMMONER_NAME
 
 
-def test_raises_with_unknown_summoner():
-    with pytest.raises(NotFoundError):
-        cassiopeia.get_summoner(UNKNOWN_SUMMONER_NAME)
-
-
-def test_raises_with_id_as_name():
-    with pytest.raises(NameError):
-        cassiopeia.get_summoner(1337)
-
-
-def test_return_type():
-    summ = cassiopeia.get_summoner(SUMMONER_NAME)
-
-    assert isinstance(summ, cassiopeia.Summoner)
+def test_unknown_summoner():
+    assert not cassiopeia.get_summoner(name=UNKNOWN_SUMMONER_NAME).exists
+    assert not cassiopeia.get_summoner(id=9999999999999).exists
 
 
 def test_equal_result_with_summoner_id():
-    from_name = cassiopeia.get_summoner(SUMMONER_NAME)
-    from_name_data = from_name._data[cassiopeia.Summoner]._dto
+    from_name = cassiopeia.get_summoner(name=SUMMONER_NAME)
     from_id = cassiopeia.get_summoner(id=from_name.id)
-    from_id_data = from_id._data[cassiopeia.Summoner]._dto
 
-    assert from_name_data == from_id_data
+    assert from_name.__dict__ == from_id.__dict__
