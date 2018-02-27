ddb799a0736e812a72bbc49981c085bdbcc89076
jason <jjmaldonis@gmail.com>
2018-02-26 18:14:10 -0600

    fixed attack damage stat for champs

--- cassiopeia/core/staticdata/champion.py
+++ cassiopeia/core/staticdata/champion.py
@@ -87,7 +87,7 @@ class SkinData(CoreData):
 
 
 class StatsData(CoreData):
-    _renamed = {"armorperlevel": "armorPerLevel", "hpperlevel": "healthPerLevel", "attackdamage": "attack_damage", "mpperlevel": "manaPerLevel", "attackspeedoffset": "attack_speed_offset", "hp": "health", "hpregenperlevel": "health_regenPerLevel", "attackspeedperlevel": "percent_attack_speedPerLevel", "attackrange": "attack_range", "attackdamageperlevel": "attack_damagePerLevel", "mpregenperlevel": "mana_regenPerLevel", "mp": "mana", "spellblockperlevel": "magicResistPerLevel", "crit": "critical_strike_chance", "mpregen": "manaRegen", "spellblock": "magicResist", "hpregen": "healthRegen", "critperlevel": "criticalStrikeChancePerLevel"}
+    _renamed = {"armorperlevel": "armorPerLevel", "hpperlevel": "healthPerLevel", "attackdamage": "attackDamage", "mpperlevel": "manaPerLevel", "attackspeedoffset": "attack_speed_offset", "hp": "health", "hpregenperlevel": "health_regenPerLevel", "attackspeedperlevel": "percent_attack_speedPerLevel", "attackrange": "attack_range", "attackdamageperlevel": "attack_damagePerLevel", "mpregenperlevel": "mana_regenPerLevel", "mp": "mana", "spellblockperlevel": "magicResistPerLevel", "crit": "critical_strike_chance", "mpregen": "manaRegen", "spellblock": "magicResist", "hpregen": "healthRegen", "critperlevel": "criticalStrikeChancePerLevel"}
 
 
 class InfoData(CoreData):
